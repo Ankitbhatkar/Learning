5511cd0134b37164da4e0f6b48286cc05ed5ddec
Ankitbhatkar <148619736+Ankitbhatkar@users.noreply.github.com>
2024-07-25 19:29:36 +0000

    change in location of file

---
+++ 02_basics/02_Arrays.js
@@ -0,0 +1,28 @@
+ let marvel_heros=["thor","ironman","spiderman"];
+ let dc_heros=["superman","flash","batrman"];
+
+//   marvel_heros.push(dc_heros);
+//  console.log(marvel_heros);
+// console.log(marvel_heros[3][1]);
+
+// let allheros=marvel_heros.concat(dc_heros);
+// console.log(allheros); //ok but not good
+
+
+// const anotherarr=[1,2,[3,4,5],7,[3,4,5,[6,7]]];
+//  const usefullanotherarr = anotherarr.flat(Infinity);
+//  console.log(usefullanotherarr);
+
+
+// console.log(Array.isArray("ankit"));
+// console.log(Array.from("ankit"));
+//console.log(Array.from({name:"raju"}));//interesting
+
+
+const score1=100;
+const score2=200;
+const score3=300;
+console.log(Array.of(score1,score2,score3));
+
+
+
