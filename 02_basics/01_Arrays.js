a19eba7e83106dc841655926b178b36ede6f6852
Ankitbhatkar <148619736+Ankitbhatkar@users.noreply.github.com>
2024-07-25 18:53:56 +0000

    change in location of file

---
+++ 02_basics/01_Arrays.js
@@ -0,0 +1,41 @@
+
+// array
+
+const myArr =['a','b','c','d',1,5,true];
+ const myArr2 = new Array(1,2,3,4);
+console.log(myArr[0]);
+
+// Arry methods
+
+// myArr2.push(5);
+// myArr2.push(6);
+// myArr2.push(7);
+
+// myArr2.pop();
+// myArr2.pop();
+
+// myArr2.unshift(9);
+// myArr2.shift();
+// console.log(myArr2);
+
+
+// console.log(myArr2.includes(2));
+// console.log(myArr2.indexOf(3));
+
+// const newarr= myArr2.join();
+// console.log(myArr2);
+// console.log(newarr);
+// console.log(typeof newarr);
+ 
+// slice & splice
+
+console.log("A" ,myArr2);
+
+console.log(myArr2.slice(1,3));
+
+console.log("B" ,myArr2);
+
+console.log(myArr2.splice(1,3));
+
+console.log("c" ,myArr2);
+
