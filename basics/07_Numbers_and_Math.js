ef8d64ed7992676d7eb00842e7089bec6da82d20
Ankitbhatkar <148619736+Ankitbhatkar@users.noreply.github.com>
2024-07-25 16:44:55 +0000

    change in location of file

--- basics/07_Numbers_and_Math.js
+++ basics/07_Numbers_and_Math.js
@@ -14,4 +14,24 @@
    console.log(hundreds.toLocaleString('en-IN'));
 
    /////////////////+++++++++++++++++++++++++ Maths===============
+   
+   console.log(Math);
+   console.log(Math.abs(-9));
+   console.log(Math.round(4.2));
+   console.log(Math.ceil(4.6));
+   console.log(Math.floor(4.6));
+   console.log(Math.min(2,4,8,1));
+   console.log(Math.max(2,4,8,1));
+
+     
+   console.log(Math.random());
+   console.log(Math.floor(Math.random()*10)+1);
+
+   
+   const min=10;
+   const max=20;
+
+   console.log(Math.floor(Math.random()*(max - min + 1))*min);
+   
+
   
