359dec75a3e47e943c02ac77451aff2138f7f5f0
Ankitbhatkar <148619736+Ankitbhatkar@users.noreply.github.com>
2024-07-27 18:17:03 +0000

    this&arrow

--- 03_basics/02_scope.js
+++ 03_basics/02_scope.js
@@ -6,13 +6,48 @@ if (true) {
     let a=10;
     const b=20;
     var c=30;
-    console.log(`inner a`,a);
+    // console.log(`inner a`,a);
     }
-    console.log(a);
+    // console.log(a);
     // console.log(b);
-    console.log(c);
+    // console.log(c);
 
 
+//nested scope
+
+function one() {
+    const username ="ankit";
+    function two() {
+          const website="youtube";
+        console.log(username);
+        
+    }
+    // console.log(website);
+   two()
+
+}
+// one()
+if (true) {
+    const username ="ankit";
+        if(username==="ankit"){
+            const website="youtube";
+            // console.log(username+website);
+        }
+        // console.log(website);
+    }
+    // console.log(username);
+
+ //+++++++++++++++++++++++++++++++interesting++++++++++++++++++++++++++++++
+ addone(5)
+function addone(value) {
+    return value+1;
+    
+}
+addtwo(2)
+const addtwo =function (value) {
+    return value+2;
+    
+}
 
 
 
---
+++ 03_basics/03_arrow.js
@@ -0,0 +1,53 @@
+const user={
+    username:"Ankit",
+    price:299,
+    massageForUser : function () {
+        console.log(`${this.username},welcome to website`);
+        // console.log(this)
+        
+    }
+    
+}
+// user.massageForUser()
+// user.username='Ram';
+// user.massageForUser()
+
+
+// console.log(this)
+
+
+// function chai(){
+// let name="ramu";
+//     console.log(this.username);
+// }
+// chai()
+
+// const chai=function (){
+//     let name="ramu";
+//         console.log(this.username);
+//     }
+//     chai()
+
+// const chai= ()=>{
+//         let name="ramu";
+//             console.log(this.username);
+//         }
+//         chai()
+
+// const addTwo =(num1,num2)=>{
+//     return num1+num2
+// }
+// console.log(addTwo(2,3));
+
+
+// const addTwo =(num1,num2)=> num1+num2
+// console.log(addTwo(2,3))
+
+// const addTwo =(num1,num2)=> (num1+num2)
+// console.log(addTwo(2,3))
+
+// const getobj=()=>({age:12})
+// console.log(getobj())
+
+const myarr=[2,3,4,5]
+myarr.forEach(()=>())
