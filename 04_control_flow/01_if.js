23a0ec6a611be8d19612d5a2bb5f6e891900c379
Ankitbhatkar <148619736+Ankitbhatkar@users.noreply.github.com>
2024-07-29 14:45:50 +0000

    if_statement

---
+++ 04_control_flow/01_if.js
@@ -0,0 +1,52 @@
+
+//if
+const usersloggedIn =true
+if (usersloggedIn) {
+    
+}
+const tempreture = 41;
+if (tempreture>50) {
+    // console.log("tempreture less than 50");
+}
+else{
+    // console.log("tempreture more than 50");
+}
+//<,>,<=,>=,==,!=,===
+
+
+
+
+// const score=200;
+// if (score>100) {
+//     const power="fly";
+//     console.log(`user power${power}`);
+    
+// }
+// console.log(`user power${power}`);
+
+
+const balance =1000;
+// if (balance>500) console.log("test"),console.log("test2");//not use
+ 
+// if (balance>500){
+//     console.log("less than");
+// }if (balance<750) {
+//     console.log("less than750");
+// } if (balance<900) {
+//     console.log("less than900");
+// } else {
+//     console.log("less than 1200");
+// }
+
+
+const userloggedIn=true;
+const debitcard=true;
+const loggedinfromgoogle=false;
+const loggedinfromemail=true;
+
+if (userloggedIn && debitcard) {
+    console.log("Allow to by course");
+}
+if (loggedinfromgoogle || loggedinfromemail) {
+    console.log("user logged in");
+}
