ed276be1ab08ca2a179a9debeb8fc816d64d4573
Ankitbhatkar <148619736+Ankitbhatkar@users.noreply.github.com>
2024-07-30 18:01:00 +0000

    while&dowhile

---
+++ 05_iterations/two.js
@@ -0,0 +1,20 @@
+let index=0;
+while (index<=10) {
+//    console.log(index);
+   index= index + 1; 
+}
+
+let myarr=["spiderman","ironman","batman","superman"]
+let arr=0;
+while (arr<myarr.length) {
+    // console.log(` value is ${myarr[arr]}`);
+    arr=arr+1;
+}
+
+
+
+let score=11;
+do {
+    console.log(`score is ${score}`);
+    score++;
+} while (score<=10);
