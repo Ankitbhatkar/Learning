cb6432acde82441e91c50ce1fbe5d4a167a4489d
Ankitbhatkar <148619736+Ankitbhatkar@users.noreply.github.com>
2024-08-01 17:44:58 +0000

    Dom notes

--- 06_DOM/one.js
+++ 06_DOM/one.js
@@ -48,3 +48,71 @@
 // 'white'
 // title.style.backgraundcolor='white'
 // 'white'
+//  ==============================================================interesting========================================================== 
+
+// title.textContent
+// 'DOM learning on Chai aur code test text'
+// title.innerText
+// 'DOM learning on Chai aur code'
+// title.innerHTML
+// 'DOM learning on Chai aur code <span style="display: none;">test text</span>'
+// ========================================================================================================================================
+// document.querySelector('h2')
+// <h2>​Lorem ipsum dolor sit.​</h2>​
+// document.querySelector('#title')
+// <h1 id=​"title" class=​"heading">​…​</h1>​
+// document.querySelector('.heading')
+// <h1 id=​"title" class=​"heading">​…​</h1>​
+// document.querySelector('input[type="password"]')
+// <input type=​"password" name id>​
+// document.querySelector('#title')
+// <h1 id=​"title" class=​"heading">​…​</h1>​
+// document.querySelector('h2')
+// <h2>​Lorem ipsum dolor sit.​</h2>​
+
+
+// let myul=document.querySelector('ul')
+//  undefined
+// let myli=myul.querySelector('li')
+// undefined
+// myli.style.color="red"
+// 'red'
+
+
+// const TempLilist=document.querySelectorAll("li")
+// undefined
+// TempLilist.style.color="red"
+// TempLilist.style.color="red"
+// VM501:1 Uncaught TypeError: Cannot set properties of undefined (setting 'color')
+//     at eval (eval at <anonymous> (one.html:1:23), <anonymous>:1:23)
+//     at <anonymous>:1:23
+// eval @ VM501:1
+// (anonymous) @ VM495:1Understand this error
+// TempLilist[0].style.color="red"
+// 'red'
+
+
+
+//  const TempLilist=document.querySelectorAll("li")
+// undefined
+// TempLilist.forEach(function(l){
+//     l.style.color="green"})
+// undefined
+
+
+// const TempClasslist=document.querySelectorAll(".list-item")
+// undefined
+// TempClasslist.forEach(function(item){
+// console.log(item);
+// })
+// VM837:2 <li class=​"list-item" style=​"color:​ green;​">​…​</li>​
+// VM837:2 <li class=​"list-item" style=​"color:​ green;​">​…​</li>​
+// VM837:2 <li class=​"list-item" style=​"color:​ green;​">​…​</li>​
+// VM837:2 <li class=​"list-item" style=​"color:​ green;​">​…​</li>​
+// undefined
+
+
+// const allH2=document.querySelectorAll("h2");
+// undefined
+// allH2[3].innerText
+// 'Website client-side usage'
