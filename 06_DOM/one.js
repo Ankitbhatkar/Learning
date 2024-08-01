44b3705b943408ca4baccbcdcd5a5031aeb95b68
Ankitbhatkar <148619736+Ankitbhatkar@users.noreply.github.com>
2024-08-01 16:31:15 +0000

    dom

---
+++ 06_DOM/one.js
@@ -0,0 +1,50 @@
+// // All DOM Selectors
+
+// // 01 August 2024
+// // 10:50
+
+// // 1 document.getElementById():
+
+
+// This function is use for selecting the element by give the name of id
+
+// Ex: document.getElementById("name of id")
+
+
+// document.getElementById('title')
+// ->  <h1 id=​"title" class=​"heading">​…​</h1>​
+// document.getElementById('title').id
+// ->   'title'
+// document.getElementById('title').class
+// -> undefined
+// document.getElementById('title').className
+// ->  'heading'
+
+
+
+
+
+
+
+// const myelement= document.getElementById("title")
+// undefined
+// myelement.style.backgraundcolor="red"
+// 'red'
+
+
+
+// document.getElementById('title').getAttribute("id")
+// ->  'title'
+// document.getElementById('title').getAttribute("class")
+// ->  'heading'
+
+// title.style.backgraundcolor='blue'
+// 'blue'
+// title.style.color='blue'
+// 'blue'
+// title.style.padding='15px'
+// '15px'
+// title.style.backgraundcolor='white'
+// 'white'
+// title.style.backgraundcolor='white'
+// 'white'
